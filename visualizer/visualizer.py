eb319f2f3c4671df6821515160993a9587091911
Kyle Hollins Wray <kyle.hollins.wray@gmail.com>
2014-08-10 19:07:27 -0400

    Working visualizer for just the LOSM map.

--- visualizer/visualizer.py
+++ visualizer/visualizer.py
@@ -22,6 +22,9 @@
 
 from __future__ import print_function
 
+import sdl2
+import sdl2.ext
+
 import sys
 sys.path.append("../../losm/losm_converter")
 from losm_converter import *
@@ -33,15 +36,20 @@ class LMDPVisualizer:
         by solving the corresponding LMDP.
     """
 
-    def __init__(self, filePrefix=None, policyFile=None):
+    def __init__(self, maxSize=1500, filePrefix=None, policyFile=None):
         """ The constructor for the LMDP class. Optionally, allow for the LOSM files
             to be loaded with the corresponding prefix. Also, optionally allow for
             the policy to be loaded.
 
             Parameters:
-                filePrefix -- The prefix for the three LOSM files.
-                policyFile -- The policy file to load.
+                maxSize     -- The max size in pixels of width or height of the window.
+                filePrefix  -- The prefix for the three LOSM files.
+                policyFile  -- The policy file to load.
         """
+
+        self.width = maxSize
+        self.height = maxSize
+        self.objSize = 20
 
         self.losm = None
         if filePrefix != None:
@@ -61,6 +69,70 @@ class LMDPVisualizer:
 
         self.losm = LOSMConverter(filePrefix)
 
+        self._compute_bounds()
+        self._adjust_nodes_and_landmarks()
+        self._create_uid_to_node_dict()
+
+
+    def _compute_bounds(self):
+        """ Compute the bounds of the LOSM object. """
+
+        self.bounds = {'xmin': float("inf"), 'ymin': float("inf"), \
+                       'xmax': float("-inf"), 'ymax': float("-inf")}
+
+        for obj in self.losm.nodes + self.losm.landmarks:
+            if self.bounds['xmax'] < obj.x:
+                self.bounds['xmax'] = obj.x
+            if self.bounds['ymax'] < obj.y:
+                self.bounds['ymax'] = obj.y
+            if self.bounds['xmin'] > obj.x:
+                self.bounds['xmin'] = obj.x
+            if self.bounds['ymin'] > obj.y:
+                self.bounds['ymin'] = obj.y
+
+        # Modify the bounds of the window based on these bounds. This
+        # preserves the apsect ratio of the map, while maximizing the
+        # window as large as possible.
+        arWindow = float(self.width) / float(self.height)
+        arMap = float(self.bounds['xmax'] - self.bounds['xmin']) / \
+                float(self.bounds['ymax'] - self.bounds['ymin'])
+
+        if arMap < arWindow:
+            self.width = int(arMap * self.height)
+        else:
+            self.height = int(arMap / self.width)
+
+
+    def _adjust_nodes_and_landmarks(self):
+        """ Adjust the location of each node and landmark based on the
+            bounds to fit in the screen.
+        """
+
+        for obj in self.losm.nodes + self.losm.landmarks:
+            obj.x = int(float(self.width) * (obj.x - self.bounds['xmin']) / \
+                            (self.bounds['xmax'] - self.bounds['xmin']))
+            obj.y = int(float(self.height) * (obj.y - self.bounds['ymin']) / \
+                            (self.bounds['ymax'] - self.bounds['ymin']))
+
+        # We must flip x and y since longitude and latitude are backwards.
+        # Don't ask me why. We also must invert the (new) y-axis since pixels
+        # are also backwards.
+        temp = None
+        for obj in self.losm.nodes + self.losm.landmarks:
+            temp = obj.x
+            obj.x = obj.y
+            obj.y = self.width - temp
+
+        temp = self.width
+        self.width = self.height
+        self.height = temp
+
+
+    def _create_uid_to_node_dict(self):
+        """ Create the UID to LOSMNode dictionary for quick edge rendering. """
+
+        self.uidToNode = {node.uid: node for node in self.losm.nodes}
+
 
     def load_policy(self, policyFile):
         """ Load the LMDP policy from the file provided.
@@ -69,4 +141,66 @@ class LMDPVisualizer:
                 policyFile -- The policy file to load.
         """
 
-        
+        pass
+
+
+    def execute(self):
+        """ Begin the visualizer. """
+
+        sdl2.ext.init()
+
+        window = sdl2.ext.Window("LMDP Visualizer",
+                                 size=(self.width, self.height))
+        window.show()
+
+        renderer = sdl2.ext.Renderer(window)
+
+        running = True
+        while running:
+            events = sdl2.ext.get_events()
+            for event in events:
+                if event.type == sdl2.SDL_QUIT:
+                    running = False
+                    break
+
+            renderer.color = sdl2.ext.Color(0, 0, 0)
+            renderer.clear()
+
+            for edge in self.losm.edges:
+                n1 = self.uidToNode[edge.uid1]
+                n2 = self.uidToNode[edge.uid2]
+
+                if edge.name == "Gray Street":
+                    renderer.color = sdl2.ext.Color(255, 0, 0)
+                else:
+                    renderer.color = sdl2.ext.Color(255, 255, 255)
+
+                renderer.draw_line([n1.x, n1.y, n2.x, n2.y])
+
+            for obj in self.losm.landmarks:
+            #for node in self.losm.nodes + self.losm.landmarks:
+                r = (obj.x - int(self.objSize / 2), \
+                     obj.y - int(self.objSize / 2), \
+                     self.objSize, \
+                     self.objSize)
+
+                renderer.color = sdl2.ext.Color(0, 255, 0)
+                renderer.draw_rect([r])
+
+            renderer.present()
+
+        sdl2.ext.quit()
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        v = LMDPVisualizer(filePrefix=sys.argv[1])
+        v.execute()
+    elif len(sys.argv) == 3:
+        v = LMDPVisualizer(filePrefix=sys.argv[1], policyFile=sys.argv[2])
+        v.execute()
+    else:
+        print("python visualizer.py " +
+              "<LOSM file prefix> " +
+              "<optional, policy file name>")
+
