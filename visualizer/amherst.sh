5c3d73372349efac5cef79951f6c051c2482acf3
Kyle Hollins Wray <kyle.hollins.wray@gmail.com>
2015-08-02 18:17:47 -0400

    Minor update to Amherst script.

--- visualizer/amherst.sh
+++ visualizer/amherst.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 cd ~/Development/losm/losm_visualizer
-optirun python visualizer.py 1600 900 1 ~/Development/lmdp/resources/amherst_ ~/Development/lmdp/policies/amherst.policy
+#optirun python visualizer.py 1600 900 1 ~/Development/lmdp/resources/amherst_ ~/Development/lmdp/policies/amherst.policy
+optirun python visualizer.py 2400 1350 1 ~/Development/lmdp/resources/amherst_ ~/Development/lmdp/policies/amherst.policy
 
